93c1cb9e60e105a91551f6f32cd812d277e338d8
StefanPluto <stefan@167pluto.com>
2023-05-14 23:54:22 +0200

    GoMysql update

--- golang/binlog_syncer.go
+++ golang/binlog_syncer.go
@@ -37,15 +37,15 @@ type canalEventHandler struct {
 	events chan<- MysqlBinlogChangeEvent
 }
 
-func (eh *canalEventHandler) OnRotate(_ *replication.RotateEvent) error {
+func (eh *canalEventHandler) OnRotate(_ *replication.EventHeader, _ *replication.RotateEvent) error {
 	return nil
 }
 
-func (eh *canalEventHandler) OnTableChanged(_ string, _ string) error {
+func (eh *canalEventHandler) OnTableChanged(_ *replication.EventHeader, _ string, _ string) error {
 	return nil
 }
 
-func (eh *canalEventHandler) OnDDL(_ mysql.Position, _ *replication.QueryEvent) error {
+func (eh *canalEventHandler) OnDDL(_ *replication.EventHeader, _ mysql.Position, _ *replication.QueryEvent) error {
 	return nil
 }
 
@@ -85,15 +85,15 @@ func (eh *canalEventHandler) OnRow(event *canal.RowsEvent) error {
 	return nil
 }
 
-func (eh *canalEventHandler) OnXID(_ mysql.Position) error {
+func (eh *canalEventHandler) OnXID(_ *replication.EventHeader, _ mysql.Position) error {
 	return nil
 }
 
-func (eh *canalEventHandler) OnGTID(_ mysql.GTIDSet) error {
-	return nil
+func (eh *canalEventHandler) OnGTID(header *replication.EventHeader, gtid mysql.GTIDSet) error {
+    return nil
 }
 
-func (eh *canalEventHandler) OnPosSynced(_ mysql.Position, _ mysql.GTIDSet, _ bool) error {
+func (eh *canalEventHandler) OnPosSynced(_ *replication.EventHeader, _ mysql.Position, _ mysql.GTIDSet, _ bool) error {
 	return nil
 }
 
